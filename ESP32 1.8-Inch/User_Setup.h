7359014607ba50001461661a82b9721d969227ff
孤独的二进制 <wangyu1026@gmail.com>
2024-05-10 14:29:16 +1000

    ESP32 1.8-Inch

---
+++ ESP32 1.8-Inch/User_Setup.h
@@ -0,0 +1,31 @@
+#define USER_SETUP_INFO "User_Setup"
+#define ST7735_DRIVER  
+      
+#define TFT_RGB_ORDER TFT_RGB 
+
+#define TFT_WIDTH  128
+#define TFT_HEIGHT 160
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   15
+#define TFT_DC    2
+#define TFT_RST  4  
+
+#define TFT_BL   32
+#define TFT_BACKLIGHT_ON HIGH
+
+#define LOAD_GLCD
+#define LOAD_FONT2 
+#define LOAD_FONT4 
+#define LOAD_FONT6
+#define LOAD_FONT7  
+#define LOAD_FONT8  
+#define LOAD_FONT8N 
+#define LOAD_GFXFF 
+#define SMOOTH_FONT
+
+
+#define SPI_FREQUENCY  27000000
+#define SPI_READ_FREQUENCY  20000000
+
